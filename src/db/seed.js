0add3e6c995368a08f6925af05bda3c6cc2e0fdb
avin <carcinogen75@gmail.com>
2018-12-02 16:02:43 +0300

    fixes

--- src/db/seed.js
+++ src/db/seed.js
@@ -2,21 +2,25 @@ import User from '../models/User';
 import Note from '../models/Note';
 import Group from '../models/Group';
 import { generateNote } from '../utils/fake';
+import { resetDB } from '../utils/db';
 
 export default async function seed() {
     // Если нет пользователей
     if (!await User.countDocuments()) {
+
+        await resetDB();
+
         // Создаем группу
         const group = await new Group({
             title: 'My group',
-        });
+        }).save();
 
         // Создаем пользователя
         const user = await new User({
             email: 'admin@email.com',
             password: 'password',
             registration: { verified: true },
-            groups: [group],
+            groups: [{group, role: 0}],
         }).save();
 
         // Персональную заметку
@@ -24,14 +28,14 @@ export default async function seed() {
             ...generateNote(),
             title: 'Personal note1',
             owner: user,
-        });
+        }).save();
 
         // Заметку группы
         await new Note({
             ...generateNote(),
             title: 'Group note1',
             group,
-        });
+        }).save();
 
         console.info('DB seeded');
     } else {
