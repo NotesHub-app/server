d0cc5c115d94ca21fe906b31529150fdec7021ff
Avin <carcinogen75@gmail.com>
2018-12-19 15:28:50 +0300

    save user improvements

--- src/routes/api/__tests__/users.js
+++ src/routes/api/__tests__/users.js
@@ -16,7 +16,8 @@ describe('users', () => {
             const response = await request(app)
                 .patch('/api/users/me')
                 .send({
-                    password: 'foobar321',
+                    oldPassword: 'qwerty123',
+                    newPassword: 'foobar321',
                 })
                 .set('Authorization', `JWT ${user.generateJWT()}`);
 
@@ -25,6 +26,35 @@ describe('users', () => {
             const userAfterUpdate = await User.findById(user._id);
 
             expect(userAfterUpdate.password).not.toBe(user.password);
+        });
+
+        test('обновление пароля с неверным старым', async () => {
+            let user = await new User({ email: 'user@email.com', password: 'qwerty123' }).save();
+            user = await User.findById(user._id);
+
+            const response = await request(app)
+                .patch('/api/users/me')
+                .send({
+                    oldPassword: 'NOT_OLD_PASSWORD',
+                    newPassword: 'foobar321',
+                })
+                .set('Authorization', `JWT ${user.generateJWT()}`);
+
+            expect(response.statusCode).toBe(422);
+        });
+
+        test('обновление пароля без старого', async () => {
+            let user = await new User({ email: 'user@email.com', password: 'qwerty123' }).save();
+            user = await User.findById(user._id);
+
+            const response = await request(app)
+                .patch('/api/users/me')
+                .send({
+                    newPassword: 'foobar321',
+                })
+                .set('Authorization', `JWT ${user.generateJWT()}`);
+
+            expect(response.statusCode).toBe(422);
         });
 
         test('обновление uiSettings', async () => {
--- src/routes/api/users.js
+++ src/routes/api/users.js
@@ -15,28 +15,43 @@ router.patch(
 
         // Валидация параметров
         check('uiSettings').optional(),
-        check('password')
+        check('oldPassword')
             .optional()
+            .custom(async (value, { req }) => {
+                const validOldPassword = await req.user.comparePassword(value);
+                if (!validOldPassword) {
+                    throw new Error('Старый пароль неверный');
+                }
+            }),
+        check('newPassword')
+            .optional()
+            .custom(async (value, { req }) => {
+                if (value && !req.body.oldPassword) {
+                    throw new Error('Требуется сначала указать старый пароль');
+                }
+            })
             .isLength({ min: 8 })
             .withMessage('Пароль должен содержать минимум 8 символов'),
         checkValidation(),
     ],
     async (req, res) => {
         const { user } = req;
-        const { uiSettings, password } = req.body;
+        const { uiSettings, oldPassword, newPassword } = req.body;
 
         if (uiSettings) {
             user.updateUiSetting(uiSettings);
         }
 
-        if (password) {
-            user.password = password;
+        if (newPassword) {
+            if (!oldPassword) {
+            }
+            user.password = newPassword;
         }
 
         await user.save();
 
         return res.json({ success: true });
-    },
+    }
 );
 
 export default router;
